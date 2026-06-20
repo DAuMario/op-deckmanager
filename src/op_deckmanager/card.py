dc205a07206f39f4fe9644fcba4ec13d91bebdc8
Mario <mario.daurelio@gmx.net>
2026-06-20 14:06:14 +0200

    Add Character class inheriting from Card

--- src/op_deckmanager/card.py
+++ src/op_deckmanager/card.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+
 @dataclass
 class Card:
+    """Base Class for all Cards in the One Piece TCG"""
     card_id: str
     name: str
     card_type: str
@@ -12,11 +14,20 @@ class Card:
     affiliations: list[str] = field(default_factory=list)
 
 
-
+@dataclass
+class Character(Card):
+    """Character Type Class, with Power, Counter, Effects etc."""
+    cost: int = 0
+    power: int = 0
+    counter: int = 0 # 0 = no counter printed on card
+    attributes: list[str] = field(default_factory=list) # usually characters only have 1 attribute, but edge cases exist
+    effects: list[str] = field(default_factory=list) # for now effects as str; gonna update later if needed
+    trigger: Optional[str] = None
+    has_blocker: bool = False
 
 
 if __name__ == "__main__":
-    luffy = Card(
+    luffy = Character(
         card_id="OP16-012",
         name="Monkey D. Luffy",
         card_type="Character",
@@ -24,5 +35,9 @@ if __name__ == "__main__":
         card_block=4,
         rarity="SR",
         affiliations=["Straw Hat Crew"],
+        cost=5,
+        power=6000,
+        counter=1000,
+        attributes=["Strike"],
     )
     print(luffy)
