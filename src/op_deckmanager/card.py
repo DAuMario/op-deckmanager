2f284fabbd4dec7981a1bb0c214069f159782844
Mario <mario.daurelio@gmx.net>
2026-06-20 15:47:01 +0200

    Add Event class inheriting from Card

--- src/op_deckmanager/card.py
+++ src/op_deckmanager/card.py
@@ -34,24 +34,37 @@ class Character(Card):
 
 @dataclass
 class Leader(Card):
+    """Leader Type Class with Life, Power, Effects etc."""
+
     life: int = 0
     power: int = 0
     attributes: list[str] = field(default_factory=list)
     effects: list[str] = field(default_factory=list)
 
 
+@dataclass
+class Event(Card):
+    """Event Type Card"""
+
+    cost: int = 0
+    main_effect: Optional[str] = None
+    counter_effect: Optional[str] = None
+    trigger: Optional[str] = None
+    counter: int = 0  # 0 = event has no counter effect
+
+
 if __name__ == "__main__":
-    luffy = Character(
-        card_id="OP16-012",
-        name="Monkey D. Luffy",
-        card_type="Character",
-        color=["Red"],
-        card_block=4,
-        rarity="SR",
-        affiliations=["Straw Hat Crew"],
-        cost=5,
-        power=6000,
-        counter=1000,
-        attributes=["Strike"],
+    buggy_event = Event(
+        card_id="OP16-057",
+        name="Captain Buggy's Our Savior!!",
+        card_type="Event",
+        color=["Blue"],
+        card_block=5,
+        rarity="C",
+        affiliations=["Impel Down"],
+        cost=1,
+        counter_effect="If you have 2 or more [Prisoner of Impel Down] cards, up to 1 of your Leader or Character cards gains +4000 Power during this Battle",
+        trigger="Draw 2 Cards and trash 1 card from your Hand",
+        counter=4000,
     )
-    print(luffy)
+    print(buggy_event)
